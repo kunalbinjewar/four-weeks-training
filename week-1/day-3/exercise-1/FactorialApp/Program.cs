c81b6950aaac38610955623c09353d55745aa51a
kunalbinjewar <binjewarkunal@gmail.com>
2023-05-10 15:45:48 +0530

    Factorial

--- week-1/day-3/exercise-1/FactorialApp/Program.cs
+++ week-1/day-3/exercise-1/FactorialApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
+
+// See https://aka.ms/new-console-template for more information
+using System;
 class Program
 {
     static void Main()
