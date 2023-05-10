f4942f9f14cbef7b97b82988268d04db3cd8b977
kunalbinjewar <binjewarkunal@gmail.com>
2023-05-10 14:52:50 +0530

    Circle1

--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Circle
+﻿
+public class Exercise5
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            // Create a Circle object and display its area and circumference
-        }
+        double r, per_cir;
+        double PI = 3.14;
+        Console.WriteLine("Input the radius of the circle : ");
+        r = Convert.ToDouble(Console.ReadLine());
+        per_cir = 2 * PI * r;
+        Console.WriteLine("Perimeter of Circle : {0}", per_cir);
+        Console.Read();
     }
 }
