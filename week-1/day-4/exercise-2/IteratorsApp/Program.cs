35d82a36c84da7d5a9c084af464a380ad64a778d
kunalbinjewar <binjewarkunal@gmail.com>
2023-05-11 12:39:53 +0530

    IteratorApp

--- week-1/day-4/exercise-2/IteratorsApp/Program.cs
+++ week-1/day-4/exercise-2/IteratorsApp/Program.cs
@@ -18,3 +18,44 @@
         }
     }
 }
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FibonacciIterator : IEnumerable<int>
+{
+    public IEnumerator<int> GetEnumerator()
+    {
+        int previousNumber = 0;
+        int currentNumber = 1;
+        int count = 0;
+
+        while (count < 10) // Generating first 10 numbers
+        {
+            yield return previousNumber;
+            int nextNumber = previousNumber + currentNumber;
+            previousNumber = currentNumber;
+            currentNumber = nextNumber;
+            count++;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FibonacciIterator fibonacciIterator = new FibonacciIterator();
+
+        foreach (int number in fibonacciIterator)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
