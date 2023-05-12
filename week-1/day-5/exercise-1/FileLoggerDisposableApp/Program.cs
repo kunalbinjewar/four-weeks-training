1d34693956e5d748f9c9dc9862462de549c62abf
kunalbinjewar <binjewarkunal@gmail.com>
2023-05-12 16:24:18 +0530

    FileLoggerApp

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -28,3 +28,40 @@
         }
     }
 }
+
+namespace FileLoggerDisposableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var logger = new FileLogger("hitarth.txt"))
+            {
+                logger.Log("My Name is Hitarth...!");
+                // some code here
+                logger.Log("I'm a fresher. Passing Out in 23.....!!");
+                logger.Dispose();
+            }
+            // Use FileLogger and dispose of it properly
+        }
+    }
+    class FileLogger : IDisposable
+    {
+        private StreamWriter _writer;
+        public FileLogger(string filePath)
+        {
+            _writer = new StreamWriter(filePath);
+            // Initialize StreamWriter instance
+        }
+        public void Dispose()
+        {
+            _writer.Dispose();
+            // Implement IDisposable pattern
+        }
+        public void Log(string message)
+        {
+            _writer?.WriteLine(message);
+            // Write message to the file
+        }
+    }
+}
