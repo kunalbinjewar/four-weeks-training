c76d43b4b9a49691c162056ffd716c64da7312a7
kunalbinjewar <binjewarkunal@gmail.com>
2023-05-12 16:36:01 +0530

    Covarience&CovarientApp

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -65,3 +65,63 @@ namespace FileLoggerDisposableApp
         }
     }
 }
+
+using System;
+using System.IO;
+// Create an interface called "IProcessor<in TInput, out TResult>" with a single method "TResult Process(TInput input)".
+public interface IProcessor<in TInput, out TResult>
+{
+    TResult Process(TInput input);
+}
+// Create a class called "StringToIntProcessor" that implements "IProcessor<string, int>" and returns the length of the input string.
+public class StringToIntProcessor : IProcessor<string, int>
+{
+    public int Process(string input)
+    {
+        return input.Length;
+    }
+}
+// Create a class called "DoubleToStringProcessor" that implements "IProcessor<double, string>" and returns the string representation of the input double
+public class DoubleToStringProcessor : IProcessor<double, string>
+{
+    public string Process(double input)
+    {
+        return input.ToString();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        StringToIntProcessor stringToIntProcessor = new StringToIntProcessor();
+        int output1 = stringToIntProcessor.Process("Abhi this side..!");
+        Console.WriteLine(output1);
+        DoubleToStringProcessor doubleToStringProcessor = new DoubleToStringProcessor();
+        string output2 = doubleToStringProcessor.Process(3.141146565546);
+        Console.WriteLine(output2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
